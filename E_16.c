277a8480c1cd39048c5e55998d9286b51832afeb
Yu_dong <1124250973@qq.com>
2018-07-04 17:09:49 +0800

    the answer program of the 16th

---
+++ E_16.c
@@ -0,0 +1,39 @@
+/*************************************************************************
+	> File Name: E_16.c
+	> Author: Yu_dong
+	> Mail: 1124250973@qq.com
+	> Created Time: 2018年04月29日 星期日 17时34分55秒
+ ************************************************************************/
+
+#include<stdio.h>
+
+#define BASE 10
+
+int get_digit_sum(int n) {
+    int sum = 0;
+    while (n) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int a[400] = {0};
+    a[0] = a[1] = 1;
+    for (int i = 0; i < 100; i++) {
+        for (int j = 1; j <= a[0]; j++) a[j] <<= 10;
+        for (int j = 1; j <= a[0]; j++) {
+            if (a[j] < BASE) continue;
+            a[j + 1] += a[j] / BASE;
+            a[j] %= BASE;
+            a[0] += (j == a[0]);
+        }
+    }
+    int sum = 0;
+    for (int i = 1; i <= a[0]; i++) sum += get_digit_sum(a[i]);
+    printf("%d\n", sum);
+    
+    
+    return 0;
+}
